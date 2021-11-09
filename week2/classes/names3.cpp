8e237243802e9c03f2c021b07af0bb92e48d3bba
Sergio <stealureye@yandex.ru>
2021-11-09 14:58:55 +0300

    .

--- week2/classes/names3.cpp
+++ week2/classes/names3.cpp
@@ -4,94 +4,95 @@
 #include <iostream>
 using namespace std;
 
+int	findNearest(const map<int, string>& tmp, int year, int yob) {
+	int res = yob - 1;
+	for (const auto& i : tmp) {
+		if (i.first <= year)
+			res = i.first;
+		if (i.first > year)
+			break ;
+	}
+	return (res);
+}
+	
+void	gimmenames(map<int, string>& tmp, int year, vector<string>& v, int yearofbirth) {
+	year--;
+	int i = findNearest(tmp, year, yearofbirth);
+	if (i >= yearofbirth) {
+		if (v.size() == 0)
+			v.push_back(tmp[i]);
+		if (v.size() > 0 && v.back() != tmp[i])
+			v.push_back(tmp[i]);
+		gimmenames(tmp, i, v, yearofbirth);
+	}
+}
+
+void	concatMe(map<int, string> name, int fn, string& res, int yob) {
+	vector<string> hist;
+	gimmenames(name, fn, hist, yob);
+	res = name[fn];
+	if (hist.size() > 0) {
+		if (hist.size() == 1) {
+			if (hist[0] != res)
+				res += " (" + hist[0] + ")";
+			hist.clear();
+		} else if (hist.size() > 1) {
+			if (hist[0] == res)
+				hist.erase(hist.begin());
+			int HistSize = hist.size();
+			res += " (";
+			for (int i = 0; i < HistSize; i++) {
+				if (i + 1 == HistSize) {
+					res += hist[i] + ")";
+					break ;
+				}
+				res += hist[i] + ", ";
+			}
+		hist.clear();
+		}
+	}
+}
+
 class Person {
 	public:
 		Person(const string& firstname, const string& lastname, int year) {
 			fname[year] = firstname;
 			lname[year] = lastname;
+			yearofbirth = year;
 		}
 		void	ChangeFirstName(int year, const string& name) {
-			fname[year] = name;
+			if (year >= yearofbirth)
+				fname[year] = name;
 		}
 		void	ChangeLastName(int year, const string& lastname) {
-			lname[year] = lastname;
+			if (year >= yearofbirth)
+				lname[year] = lastname;
 		}
 		string	GetFullName(int year) const {
-			int fn = findNearest(fname, year);
-			int ln = findNearest(lname, year);
-			if (fn == -1 && ln == -1)
+			int fn = findNearest(fname, year, yearofbirth);
+			int ln = findNearest(lname, year, yearofbirth);
+			map<int, string> fnm = fname;
+			map<int, string> lnm = lname;
+
+			if (fn < yearofbirth && ln < yearofbirth)
 				return ("No person");
-			if (ln == -1)
-				return (fname[fn] + " with unknown last name");
-			else if (fn == -1)
-				return (lname[ln] + " with unknown first name");
-			return (fname[fn] + " " + lname[ln]);
+			return (fnm[fn] + " " + lnm[ln]);
 		}
-		string	GetFullNameWithHistory(int year) {
+		string	GetFullNameWithHistory(int year) const {
 			string resF = "";
-			int fn = findNearest(fname, year);
-			if (fn > 0)
-				concatMe(fname, fn, resF);
+			int fn = findNearest(fname, year, yearofbirth);
+			if (fn >= yearofbirth)
+				concatMe(fname, fn, resF, yearofbirth);
 			string resL = "";
-			int ln = findNearest(lname, year);
-			if (ln > 0)
-				concatMe(lname, ln, resL);
-			if (fn == -1 && ln == -1)
+			int ln = findNearest(lname, year, yearofbirth);
+			if (ln >= yearofbirth)
+				concatMe(lname, ln, resL, yearofbirth);
+			if (fn < yearofbirth && ln < yearofbirth)
 				return ("No person");
-			if (ln == -1)
-				return (resF + " with unknown last name");
-			else if (fn == -1)
-				return (resL + " with unknown first name");
 			return (resF + " " + resL);
 		}
-	private:
-		void	concatMe(map<int, string>& name, int fn, string& res) {
-				vector<string> hist(0);
-				gimmenames(name, fn, hist);
-				res = name[fn];
-				if (hist.size() > 0) {
-					for (int i = 0; i < hist.size() - 1; i++) {
-						if (hist[i] == hist[i + 1])
-							hist.erase(hist.begin() + i);
-					}
-					if (hist.size() == 1) {
-						if (hist[0] != res)
-							res += " (" + hist[0] + ")";
-						hist.clear();
-					} else if (hist.size() > 1) {
-						if (hist[0] == res)
-							hist.erase(hist.begin());
-						int HistSize = hist.size();
-						res += " (";
-						for (int i = 0; i < HistSize; i++) {
-							if (i + 1 == HistSize) {
-								res += hist[i] + ")";
-								break ;
-							}
-							res += hist[i] + ", ";
-						}
-						hist.clear();
-					}
-				}
-		}
-		void	gimmenames(map<int, string>& tmp, int year, vector<string>& v) {
-			year--;
-			int i = findNearest(tmp, year);
-			if (i != -1) {
-				v.push_back(tmp[i]);
-				gimmenames(tmp, i, v);
-			}
-		}
-		int	findNearest(const map<int, string>& tmp, int year) const{
-			int res = -1;
-			for (const auto& i : tmp) {
-				if (i.first <= year)
-					res = i.first;
-				if (i.first > year)
-					break ;
-			}
-			return (res);
-		}
-		map<int, string> fname;
-		map<int, string> lname;
+		private:
+		map<int, string>	fname;
+		map<int, string>	lname;
+		int			yearofbirth;
 };
