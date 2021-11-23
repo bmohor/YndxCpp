3c659727dd708d4a64b1f763aeb0cc5f00dbb75f
Sergio <stealureye@yandex.ru>
2021-11-24 02:09:11 +0300

    Almost

--- solution.cpp
+++ solution.cpp
@@ -48,7 +48,7 @@ class Database {
 		bool DeleteEvent(const Date& date, const std::string& event) {
 			if (clndr.count(date) > 0) {
 				auto res = std::find(begin(clndr[date]), end(clndr[date]), event);
-				if (res == std::end(clndr[date])) {
+				if (res != end(clndr[date])) {
 					clndr[date].erase(event);
 					if (clndr[date].size() == 0)
 						clndr.erase(date);
@@ -56,7 +56,7 @@ class Database {
 					return 1;
 				}
 			}
-			std::cout << "Event not found" << std::endl;
+			throw std::invalid_argument("Event not found");
 			return 0;
 		}
 		int  DeleteDate(const Date& date) {
@@ -94,15 +94,50 @@ class Database {
 	private:
 		std::map<Date, std::set<std::string>> clndr;
 };
-
-void	dataCheck(int& a, int& b, int& c, int code) {
-	char x, z;
+void	checkdig(char& tmp, std::string res) {
 	std::string event;
-	std::cin >> a >> x >> b >> z >> c;
+	if (std::cin.peek() == '+' || std::cin.peek() == '-') {
+		std::cin >> tmp;
+		res += tmp;
+	}
+	if (isdigit(std::cin.peek()) == 0) {
+		std::string t;
+		std::cin >> t;
+		res += t;
+		getline(std::cin, event);
+		throw std::invalid_argument("Wrong date format: " + res);
+	}
+}
+void	dataCheck(int& a, int& b, int& c, int code) {
+	std::string res;
+	char x, z, tmp;
+	std::string event;
+	std::cin.ignore(1);
+	checkdig(tmp, res);
+	std::cin >> a;
+	res += std::to_string(a);
+	if (tmp == '-')
+		a*= -1;
+	tmp = '+';
+	std::cin >> x;
+	res += x;
+	checkdig(tmp, res);	
+	std::cin >> b;
+	res += std::to_string(b);
+	if (tmp == '-')
+		b *= -1;
+	tmp = '+';
+	std::cin >> z;
+	res += z;
+	checkdig(tmp, res);
+	std::cin >> c;
+	res += std::to_string(c);
+	if (tmp == '-')
+		c *= -1;
 	if (code == 1 && (std::cin.peek() != ' ' || x != '-' || z != '-')) {
 		if (std::cin.peek() != '\n' || x != '-' || z != '-') {
 			std::string tmp;
-			std::string t = "";	
+			std::string t = "";
 			if (std::cin.peek() != ' ') {
 				if (std::cin.peek() != '\n')
 					std::cin >> t;
@@ -133,48 +168,35 @@ void	dataCheck(int& a, int& b, int& c, int code) {
 }	
 
 void findMe(Database& db) {
-	try {
-		int a, b, c;
-		std::string event;
-		dataCheck(a, b, c, 1);
-		Date date(a, b, c);
-		db.Find(date);
-	} catch(const std::invalid_argument& ex) {
-		std::cout << ex.what() << std::endl;
-	}
-
+	int a, b, c;
+	std::string event;
+	dataCheck(a, b, c, 1);
+	Date date(a, b, c);
+	db.Find(date);
 }
 
 void del(Database& db) {
-	try {
-		int a, b, c;
-		std::string event;
-		dataCheck(a, b, c, 1);
-		Date date(a, b, c);
-		if (std::cin.peek() == '\n') {
-			int res = db.DeleteDate(date);
-			std::cout << "Deleted " << res << " events" << std::endl;
-		}
-		else {
-			std::cin >> event;
-			db.DeleteEvent(date, event);
-		}
-	} catch(const std::invalid_argument& ex) {
-		std::cout << ex.what() << std::endl;
+	int a, b, c;
+	std::string event;
+	dataCheck(a, b, c, 1);
+	Date date(a, b, c);
+	if (std::cin.peek() == '\n') {
+		int res = db.DeleteDate(date);
+		std::cout << "Deleted " << res << " events" << std::endl;
+	}
+	else {
+		std::cin >> event;
+		db.DeleteEvent(date, event);
 	}
 }
 
 void add(Database& db) {
-	try {
-		int a, b, c;
-		std::string event;
-		dataCheck(a, b, c, 0);
-		Date date(a, b, c);
-		std::cin >> event;
-		db.AddEvent(date, event);
-	} catch(const std::invalid_argument& ex) {
-		std::cout << ex.what() << std::endl;
-	}
+	int a, b, c;
+	std::string event;
+	dataCheck(a, b, c, 0);
+	Date date(a, b, c);
+	std::cin >> event;
+	db.AddEvent(date, event);
 }
 
 int main() {
@@ -182,19 +204,23 @@ int main() {
 	
 	std::string command;
 	while (1) {
-		std::cin >> command;
-		if (command == "\0")
-			continue;
-		else if (command == "Add") {
-			add(db);
-		} else if (command == "Del") {
-			del(db);
-		} else if (command == "Find") {
-			findMe(db);
-		} else if (command == "Print") {
-			db.Print();
-		} else {
-			std::cout << "Unknown command: " + command << std::endl;
+		try {
+			std::cin >> command;
+			if (command == "\0")
+				continue;
+			else if (command == "Add")
+				add(db);
+			else if (command == "Del")
+				del(db);
+			else if (command == "Find")
+				findMe(db);
+			else if (command == "Print")
+				db.Print();
+			else
+				throw std::invalid_argument("Unknown command: " + command);
+		} catch(const std::invalid_argument& ex) {
+			std::cout << ex.what() << std::endl;
+			return 0;
 		}
 	}
 	return 0;
